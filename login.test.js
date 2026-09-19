ac7dc8d231cb82010d39c3a94cb5b31427fac829
cththanh1011 <cththanh1011@gmail.com>
2026-09-19 10:25:13 +0700

    Fix login test cases for username and password

--- login.test.js
+++ login.test.js
@@ -1,11 +1,11 @@
 const login = require("./login");
 
 test("Login with correct username and password", () => {
-    expect(login("admin", "123")).toBe(true);
+    expect(login("admin", "1234")).toBe(true);
 });
 
 test("Login with incorrect password", () => {
-    expect(login("admin", "1234")).toBe(false);
+    expect(login("admin", "123")).toBe(false);
 });
 
 test("Login with incorrect username", () => {
