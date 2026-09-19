38f9328d2e150723f7b2eebec22fa8a247002487
cththanh1011 <cththanh1011@gmail.com>
2026-09-19 10:26:32 +0700

    Fix test case for login with correct password

--- login.test.js
+++ login.test.js
@@ -1,7 +1,7 @@
 const login = require("./login");
 
 test("Login with correct username and password", () => {
-    expect(login("admin", "1234")).toBe(true);
+    expect(login("admin", "123")).toBe(true);
 });
 
 test("Login with incorrect password", () => {
