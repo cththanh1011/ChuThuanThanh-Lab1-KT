d3aab78da4e8b7253c61bb7e61a577720a5a720d
cththanh1011 <cththanh1011@gmail.com>
2026-09-19 10:32:24 +0700

    Update test for incorrect password case

--- login.test.js
+++ login.test.js
@@ -5,7 +5,7 @@ test("Login with correct username and password", () => {
 });
 
 test("Login with incorrect password", () => {
-    expect(login("admin", "123")).toBe(false);
+    expect(login("admin", "1234")).toBe(false);
 });
 
 test("Login with incorrect username", () => {
